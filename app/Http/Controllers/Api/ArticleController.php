7bbd4d4355e7e00835ea6faa7db5f20be7c02348
Oscar Franco <oscarmunnoz160897@gmail.com>
2023-12-08 22:42:14 -0600

    Create ArticleController.php

---
+++ app/Http/Controllers/Api/ArticleController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ArticleResource;
+use Illuminate\Http\Request;
+
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    public function show(Article $article)
+    {
+        return ArticleResource::make($article);
+    }
+}
